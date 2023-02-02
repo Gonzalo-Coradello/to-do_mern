aa5e557f12af9827de2dde78cfa1f964f269bf71
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-02-02 12:23:02 -0300

    testing solution

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const tasksRouter = require('./routes/tasks.router')
 const usersRouter = require('./routes/users.router')
+let cors = require("cors");
 
 const app = express()
 
 app.use(express.json())
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
